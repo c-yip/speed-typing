0541b93130e45cf9c751a04ca9e4c9ce276fad68
Christian <christianyip92@gmail.com>
2022-10-19 10:51:03 -0700

    Add state and function to track textarea value.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [text, setText] = useState('');
+  
+  function handleChange(e) {
+    const {value} = e.target;
+    setText(value);
+  }
+
   return (
     <div className="App">
       <h1 className='title'>Speed Typing</h1>
-      <textarea></textarea>
+      <textarea
+        name='text'
+        value={text}
+        onChange={handleChange}
+      ></textarea>
       <h4 className='time-remaining'>Time Remaining:</h4>
       <button className='start-button'>Start</button>
       <h1 className='word-count'>Word Count:</h1>
